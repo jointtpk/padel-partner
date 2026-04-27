ecadc9f33b4c19eb6426991e629fc05ed0b7933c
jointtpk <jointtpk@users.noreply.github.com>
2026-04-27 11:26:35 +0000

    Firebase build fixes: multidex + minSdk 23 + cleaner google-services

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,14 +21,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.padelpartner.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23 // Firebase Auth requires 23+
         targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
