ba13c80b5b356cb6f3ac7b0fc02ef1f2944992a4
jointtpk <jointtpk@users.noreply.github.com>
2026-04-28 12:18:13 +0500

    1.8

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/claude/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/claude/padel_partner"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Bronze Digitals\Clients\Padel Partner - Mujtaba\padel-partner"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
